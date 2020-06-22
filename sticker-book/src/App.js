971e12f8e097c12da0079cab8690cd0c53bcd573
David Downie <daviddownie@gmail.com>
2020-06-22 18:33:05 -0400

    initial setup

---
+++ sticker-book/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+     
+    }
+  }
+
+  componentDidMount(){
+ 
+  }
+  
+  
+  render(){
+
+    return (
+      <>
+        <header>
+          <h1>Sticker Book</h1>
+        </header>
+
+      </>
+    );
+  }
+}
+
+export default App;
