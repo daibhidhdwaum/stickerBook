17598371d75b5943c87bcf24fc1c363cda35f7ac
David Downie <daviddownie@gmail.com>
2020-06-23 15:37:52 -0400

    added on click function to display images

--- sticker-book/src/App.js
+++ sticker-book/src/App.js
@@ -3,21 +3,23 @@ import axios from "axios";
 import "./App.css";
 import Sticker from "./Sticker"
 
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      charactersInfo: []
+      charactersInfo: [],
+      visible: false,
+      stickerClicked: []
     };
   }
 
   componentDidMount() {
     axios({
-      url: "https://rickandmortyapi.com/api/character/[1,2,3]",
+      url: "https://rickandmortyapi.com/api/character/[1,2,3,4,5,7,20,47,103,118,162,171,196,242,244,252,259,265,306,329,353,372]",
       method: "GET",
       responseType: "JSON",
     }).then((response) => {
-      console.log(response);
       response = response.data;
 
       this.setState({
@@ -26,23 +28,58 @@ class App extends Component {
     });
   }
 
+  handleClick = (event) => {
+    event.preventDefault();
+  
+    let copyOfVisible = this.state.visible;
+    copyOfVisible = true;
+    console.log(event.target.id)
+
+    const sticker = event.target.childNodes[1].classList;
+
+    sticker.remove("hidden");
+   
+    sticker.add("visible");
+  
+
+
+    console.log(event.target.childNodes[1].classList)
+  
+      console.log(this.state.visible)
+  }
+
   render() {
     return (
       <>
         <header>
-          <h1>Sticker Book</h1>
+          <div></div>
+          {/* div stores background image */}
+          <h1>
+            <span className="visuallyHidden">Rick and Morty</span> Sticker Book
+          </h1>
         </header>
-        {this.state.charactersInfo.map((character) => {
-          {console.log(character)}
-          return <>
-          <Sticker 
-            key={character.id}
-            name={character.name}
-            image={character.image}
-            species={character.species}/>
-          </>;
-        })}
-        |
+        <main>
+          {this.state.charactersInfo.map((character, index) => {
+            const key = character.id.toString();
+
+            return (
+              
+              <Sticker
+                  stickerNumber={index + 1}
+                  key={key}
+                  id={key}
+                  name={character.name}
+                  image={character.image}
+                  species={character.species}
+                  status={character.status}
+                  planet={character.origin.name}
+                  value={this.state.stickerClicked}
+                  handleClick={(event) => this.handleClick(event)}
+                />
+              
+              );
+          })}
+        </main>
       </>
     );
   }
--- sticker-book/src/Sticker.js
+++ sticker-book/src/Sticker.js
@@ -1,15 +1,33 @@
 import React from "react";
 
 const Sticker = (props) => {
-    return(
-        <>
-            <img src={props.image} alt={props.name} />
-            <h2>name: {props.name}</h2>
-            <p>species: {props.species}</p>
+    console.log(props)
+  return (
+    <div className="flex stickerContainer" key={props.id}>
+      <button
+        className="imageContainer"
+        onClick={props.handleClick}
+        id={props.id} stickerNumber={props.stickerNumber}
+      >{props.stickerNumber}
+        <img
+          className="sticker hidden"
+          src={props.image}
+          alt={props.name}
+          onClick={props.handleClick}
+          id={props.id}
+        />
+      </button>
+      <div>
+        <h2>name: {props.name}</h2>
+        <p>species: {props.species}</p>
+        <p>status: {props.status}</p>
+        <p>Planet: {props.planet}</p>
+      </div>
+    </div>
+  );
+};
 
 
-        </>
-    )
-}
+
 
 export default Sticker;
