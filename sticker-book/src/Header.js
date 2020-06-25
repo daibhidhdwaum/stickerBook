94efd2a085ac761aab2c0124826f8079284af4e4
David Downie <daviddownie@gmail.com>
2020-06-24 22:22:47 -0400

    removed arrows

--- sticker-book/src/Header.js
+++ sticker-book/src/Header.js
@@ -5,17 +5,9 @@ const Header = () => {
     <header>
       <div className="backgroundImage"></div>
       {/* div stores background image */}
-      <div className="entryBar">
-        <div className="arrow">
-          <i className="fas fa-arrow-down" ></i>
-        </div>
-        <h1>
-          <span className="visuallyHidden">Rick and Morty</span> Sticker Book
-        </h1>
-        <div className="arrow">
-          <i className="fas fa-arrow-down"></i>
-        </div>
-      </div>
+      <h1>
+        <span className="visuallyHidden">Rick and Morty</span> Sticker Book
+      </h1>
     </header>
   );
 };
